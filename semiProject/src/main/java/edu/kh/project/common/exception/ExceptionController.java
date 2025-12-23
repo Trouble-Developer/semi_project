b2b0096fc83b9063d3b29f5b2826956e05e216e6
zenoo97 <jaehun4086@naver.com>
2025-12-23 21:54:56 +0900

    [Update] 404, 500 페이지

---
+++ semiProject/src/main/java/edu/kh/project/common/exception/ExceptionController.java
@@ -0,0 +1,36 @@
+package edu.kh.project.common.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+
+@ControllerAdvice 
+public class ExceptionController {
+
+	/** dev. 안재훈
+	 * 404 에러 발생시 404 페이지로 forward
+	 * @return
+	 */
+	@ExceptionHandler(NoResourceFoundException.class)
+	// 404 오류
+	public String notFound() { 
+		return "error/404";
+	}
+
+	/** dev. 안재훈
+	 * 500 에러 발생시 500 페이지로 forward
+	 * @param model
+	 * @param e
+	 * @return
+	 */
+	@ExceptionHandler(Exception.class)
+	public String allExceptionHandler(Model model, Exception e) {
+		// 500 error
+		e.printStackTrace();
+		model.addAttribute("e", e);
+		return "error/500"; 
+	}
+
+}
