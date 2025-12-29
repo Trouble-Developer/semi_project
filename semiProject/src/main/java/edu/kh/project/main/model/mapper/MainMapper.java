32ed73dcdef6e95dd35410f87f2ade1092a20444
조창래 <chochanglae1223@gmail.com>
2025-12-29 16:09:06 +0900

    Squashed commit of the following:
    commit e9dd9cc693fed09f33a301577c25c0700fc7d54a
    Merge: 00cc7c0 6f19291
    Author: jaehoon <jaehun4086@naver.com>
    Date:   Mon Dec 29 16:08:25 2025 +0900
    
        Merge pull request #46 from Trouble-Developer/developer_AJH
    
        [Add] 메인페이지 작업 -> 머지테스트 전
    
    commit 6f192911abfaac3a9e4b2ca69d91f01284811cb2
    Author: zenoo97 <jaehun4086@naver.com>
    Date:   Mon Dec 29 16:07:14 2025 +0900
    
        [Add] 메인페이지 작업 -> 머지테스트 전

--- semiProject/src/main/java/edu/kh/project/main/model/mapper/MainMapper.java
+++ semiProject/src/main/java/edu/kh/project/main/model/mapper/MainMapper.java
@@ -1,12 +1,15 @@
 package edu.kh.project.main.model.mapper;
 
+
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import edu.kh.project.board.model.dto.Board;
 
 @Mapper
 public interface MainMapper {
 
+	List<Board> getVolunteerList(int boardNo);
 
 }
