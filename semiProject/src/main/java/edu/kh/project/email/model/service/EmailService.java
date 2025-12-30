6e6687bf4084d0f77e701cd1dc1cccbe34bc6941
조창래 <chochanglae1223@gmail.com>
2025-12-31 03:48:32 +0900

    Delete semiProject directory

--- semiProject/src/main/java/edu/kh/project/email/model/service/EmailService.java
+++
@@ -1,8 +0,0 @@
-package edu.kh.project.email.model.service;
-
-import java.util.Map;
-
-public interface EmailService {
-    String sendEmail(String string, String email);
-    int checkAuthKey(Map<String, String> paramMap);
-}
